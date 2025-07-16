8ef73d7099c5b77a268a81fcbb6b4939a5ea8931
Ephram Cola Jacquin <colajacquin.e@northeastern.edu>
2025-07-16 11:12:43 +1000

    feat(secrets): new secrets/token loader

---
+++ secrets/secrets_loader.py
@@ -0,0 +1,28 @@
+import os
+import dotenv
+
+class SecretsLoader:
+    """Contains helper methods to retrieve local secrets."""
+
+    @staticmethod
+    def get_token(token_name :str, token_path :str=".env") -> str:
+        """Retrieves a token either locally or remotely (if not local).
+
+        Args:
+            token_name (str): name of the token to retrieve
+            token_path (str): path to the token (.env) file if needed (default is '.env')
+
+        Returns:
+            str: the token retrieved
+        """
+        if dotenv.load_dotenv(token_path):
+            print(f"Loaded {token_name} from token file: {token_path}")
+        else:
+            raise ImportError(
+                f"Could not load token: {token_name}, check if token exists in given token file."
+            )
+
+        if (os.environ.get(token_name)):
+            return str(os.getenv(token_name))
+        else:
+            raise OSError("Could not retrieve token.") 
